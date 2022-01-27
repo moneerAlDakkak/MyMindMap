2c3938f151657e3f2eb27917ea3d246149dfe76f
Moneer Al-Dakkak <77343614+moneerAlDakkak@users.noreply.github.com>
2022-01-27 13:15:31 +0200

    Add files via upload

---
+++ precache-manifest.f5bdbe2abc2987607818b10206bc2dd6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b180eb5a7237238cfc8",
+    "url": "/MyMindMap/css/app.d9335d4c.css"
+  },
+  {
+    "revision": "a4c73aafe14bb1b75467",
+    "url": "/MyMindMap/css/chunk-125c660b.55678836.css"
+  },
+  {
+    "revision": "71e964cb3f20761d468215e06c2d0fa5",
+    "url": "/MyMindMap/index.html"
+  },
+  {
+    "revision": "5b180eb5a7237238cfc8",
+    "url": "/MyMindMap/js/app.5176bee5.js"
+  },
+  {
+    "revision": "a4c73aafe14bb1b75467",
+    "url": "/MyMindMap/js/chunk-125c660b.663ba4c6.js"
+  },
+  {
+    "revision": "49ecf82fa90ce839d705",
+    "url": "/MyMindMap/js/chunk-vendors.a7631260.js"
+  },
+  {
+    "revision": "72ed0d4820980a6a965609b57d75071e",
+    "url": "/MyMindMap/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/MyMindMap/robots.txt"
+  }
+]);
